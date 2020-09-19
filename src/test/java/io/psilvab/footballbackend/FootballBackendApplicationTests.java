81ed31552032f639e6125a17b48d18873d90f412
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-09-19 20:28:14 -0300

    Initial Commit

---
+++ src/test/java/io/psilvab/footballbackend/FootballBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.psilvab.footballbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
